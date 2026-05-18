380ef25407bde51931087241ae53b6b23dc4c798
Simone Di Luzio <simorex@gmail.com>
2026-05-18 20:57:18 +0200

    First review

--- server.js
+++ server.js
@@ -47,19 +47,50 @@ const divisions = [
     description: 'EURACCIAI opera nel campo degli utensili da taglio e parti di usura/componenti in carburo di tungsteno da più di cinquanta anni.',
     paragraphs: [
       'Forniamo ad aziende in Italia, in Europa e nel mondo materiali e utensili per i processi di produzione più impegnativi per la asportazione di truciolo, lo stampaggio, la scordonatura di tubi, la marcatura di tubi e di componenti meccanici, la saldatura a resistenza, la sabbiatura con materiali abrasivi, e per le applicazioni che richiedono resistenza all\'usura e alle sollecitazioni meccaniche, bassi attriti e elevate durezze o elevata conduttività.',
-      "La nostra esperienza e la nostra approfondita conoscenza tecnica ci permette di fornire l'utensile, il componente o il materiale più adatto a raggiungere gli obiettivi in termini di qualità, costo e produttività."
+      "La nostra esperienza e la nostra approfondita conoscenza tecnica ci permette di fornire l'utensile, il componente o il materiale più adatto a raggiungere gli obiettivi in termini di qualità, costo e produttività.",
+      'Euracciai dispone inoltre di materiali a magazzino e di soluzioni su disegno per forniture rapide e progetti personalizzati, con attenzione a continuità produttiva e tracciabilità.'
     ],
     products: [
-      'ALBROMET Attrezzature per curvatubi',
-      'Elettrodi per saldatura a resistenza',
-      'Lame per troncatura tubi e profilati',
-      'LEGHE BRONZO-ALLUMINIO ALBROMET',
-      'ALBROMET Leghe di rame ad alta conducibilità',
-      'Marcatura industriale',
-      'Tungsteno, Molibdeno, Rame e leghe',
-      'Ugelli in carburo di tungsteno - boro - silicio',
-      'Boccole e parti di usura in metallo duro',
-      'Utensili speciali in metallo duro per asportazione di truciolo'
+      {
+        title: 'ALBROMET Attrezzature per curvatubi',
+        description: 'Anime omnidirezionali e componenti modulari per curvatubi CNC in leghe speciali ad alte prestazioni.'
+      },
+      {
+        title: 'Elettrodi per saldatura a resistenza',
+        description: 'Elettrodi su disegno in leghe di rame e tungsteno, con controlli qualità e tracciabilità.'
+      },
+      {
+        title: 'Lame per troncatura tubi e profilati',
+        description: 'Lame verticali e orizzontali per linee di taglio tubi, anche per acciai inox.'
+      },
+      {
+        title: 'LEGHE BRONZO-ALLUMINIO ALBROMET',
+        description: 'Leghe con alta resistenza all\'usura e basse frizioni per componenti di impianto e macchine.'
+      },
+      {
+        title: 'ALBROMET Leghe di rame ad alta conducibilità',
+        description: 'Materiali per stampi ed elettrodi con elevata conducibilità termica e buona durezza.'
+      },
+      {
+        title: 'Marcatura industriale',
+        description: 'Stampi incisi, punzoni, caratteri e sistemi di marcatura per metalli e tubi.'
+      },
+      {
+        title: 'Tungsteno, Molibdeno, Rame e leghe',
+        description: 'Materiali in barre, lamiere e filo disponibili a stock, anche per realizzazioni a disegno.'
+      },
+      {
+        title: 'Ugelli in carburo di tungsteno - boro - silicio',
+        description: 'Ugelli ad alta resistenza per applicazioni abrasive e processi ad alta usura.'
+      },
+      {
+        title: 'Boccole e parti di usura in metallo duro',
+        description: 'Componenti antiusura per ambienti severi con elevate sollecitazioni meccaniche.'
+      },
+      {
+        title: 'Utensili speciali in metallo duro per asportazione di truciolo',
+        description: 'Utensili personalizzati per incrementare qualità, produttività e durata in esercizio.'
+      }
     ]
   },
   {
@@ -72,15 +103,34 @@ const divisions = [
     image: '/img/division-macsi.svg',
     description: 'La divisione è specializzata in materiali avanzati per applicazioni industriali, come ceramiche tecniche, leghe resistenti alla corrosione e alle alte temperature, materiali antiusura e componenti speciali per impianti.',
     paragraphs: [
-      'Le competenze tecniche relative alla conoscenza delle applicazioni, dei materiali e dei componenti, rendono EURACCIAI un ottimo partner per sviluppare insieme ai nostri clienti le soluzioni più appropriate.'
+      'Le competenze tecniche relative alla conoscenza delle applicazioni, dei materiali e dei componenti, rendono EURACCIAI un ottimo partner per sviluppare insieme ai nostri clienti le soluzioni più appropriate.',
+      'La divisione supporta la scelta del materiale e del componente più idoneo per durata, affidabilità, manutenzione e costi complessivi di esercizio.'
     ],
     products: [
-      'COORSTEK - USA',
-      'STEINHAUS - Germania: griglie e cilindri filtranti di precisione',
-      'STEINHAUS - Germania: nastri trasportatori laminati per forni a tunnel',
-      'ULBRICH - USA',
-      'HARALD PIHL - Svezia',
-      'ABREMA - Grecia'
+      {
+        title: 'COORSTEK - USA',
+        description: 'Produttore leader di ceramiche tecniche con ampia gamma di composizioni ingegneristiche.',
+        url: 'https://www.coorstek.com'
+      },
+      {
+        title: 'STEINHAUS - Germania',
+        description: 'Griglie e cilindri filtranti di precisione; nastri trasportatori laminati per forni a tunnel.',
+        url: 'https://www.steinhaus-gmbh.de'
+      },
+      {
+        title: 'ULBRICH - USA',
+        description: 'Nastri, lamine e fili speciali in acciai inox e leghe ad alte prestazioni.',
+        url: 'https://www.ulbrich.com'
+      },
+      {
+        title: 'HARALD PIHL - Svezia',
+        description: 'Stock europeo di leghe speciali e titanio per applicazioni industriali critiche.',
+        url: 'https://www.haraldpihl.com'
+      },
+      {
+        title: 'ABREMA - Grecia',
+        description: 'Componenti e materiali speciali per applicazioni tecniche e impiantistiche.'
+      }
     ]
   },
   {
@@ -92,13 +142,105 @@ const divisions = [
     email: 'divmac@euracciai.it',
     image: '/img/division-divmac.svg',
     description: 'La conoscenza approfondita dei processi tecnologici ci permette di analizzare le esigenze tecniche e di produzione e quindi di proporre le migliori soluzioni in relazione alle caratteristiche del componente da produrre, e alle esigenze di flessibilità e produttività richieste per il progetto.',
-    paragraphs: [],
+    paragraphs: [
+      'La conoscenza approfondita dei processi tecnologici ci permette di analizzare esigenze tecniche e produttive e proporre soluzioni mirate in base al componente da realizzare.',
+      'L\'obiettivo è bilanciare flessibilità, produttività e qualità del processo, con configurazioni adatte sia a linee automatiche sia a contesti ad alta variabilità.'
+    ],
     products: [
-      'FELSS GROUP GmbH - Germania',
-      'WEIL-TECHNOLOGY - Germania',
-      'WF MASCHINENBAU UND BLECHFORMTECHNIK - Germania',
-      'LESSMANN GmbH - Germania'
+      {
+        title: 'FELSS GROUP GmbH - Germania',
+        description: 'Macchine per rullatura e deformazione di tubi, barre e filo; presse orizzontali e sistemi di produzione.',
+        url: 'https://www.felss.com'
+      },
+      {
+        title: 'WEIL-TECHNOLOGY - Germania',
+        description: 'Macchine e linee per formatura, taglio e giunzione, anche in configurazioni turn-key.',
+        url: 'https://www.weil-engineering.de'
+      },
+      {
+        title: 'WF MASCHINENBAU UND BLECHFORMTECHNIK - Germania',
+        description: 'Macchine di flow forming per pulegge, componenti trasmissione, dischi, ruote e bombole gas.',
+        url: 'https://www.wf-maschinenbau.com'
+      },
+      {
+        title: 'LESSMANN GmbH - Germania',
+        description: 'Spazzole industriali per sbavatura e finitura metalli, pipeline-welding e applicazioni speciali.',
+        url: 'https://www.lessmann.com'
+      }
     ]
+  }
+];
+
+const partners = [
+  {
+    division: 'MDU',
+    name: 'ALBROMET',
+    country: 'Germania',
+    description: 'Leghe bronzo-alluminio e leghe di rame ad alta conducibilità per utensili, stampi e componenti tecnici.',
+    url: 'https://www.albromet.de'
+  },
+  {
+    division: 'MDU',
+    name: 'NEW-FORM TOOLS',
+    country: 'Canada',
+    description: 'Lame per troncatura tubi e profilati, con soluzioni dedicate alle linee di taglio ad alta produttività.',
+    url: 'https://www.newformtools.com'
+  },
+  {
+    division: 'MACSI',
+    name: 'COORSTEK',
+    country: 'USA',
+    description: 'Materiali ceramici avanzati per applicazioni industriali ad alte prestazioni.',
+    url: 'https://www.coorstek.com'
+  },
+  {
+    division: 'MACSI',
+    name: 'STEINHAUS',
+    country: 'Germania',
+    description: 'Soluzioni per filtrazione di precisione e nastri laminati per forni a tunnel.',
+    url: 'https://www.steinhaus-gmbh.de'
+  },
+  {
+    division: 'MACSI',
+    name: 'ULBRICH',
+    country: 'USA',
+    description: 'Nastri, fogli e fili di metalli speciali per processi industriali avanzati.',
+    url: 'https://www.ulbrich.com'
+  },
+  {
+    division: 'MACSI',
+    name: 'HARALD PIHL',
+    country: 'Svezia',
+    description: 'Ampia disponibilità di leghe speciali e titanio per applicazioni critiche.',
+    url: 'https://www.haraldpihl.com'
+  },
+  {
+    division: 'DIVMAC',
+    name: 'FELSS GROUP',
+    country: 'Germania',
+    description: 'Tecnologie di deformazione e lavorazione per componenti tubolari e metallici.',
+    url: 'https://www.felss.com'
+  },
+  {
+    division: 'DIVMAC',
+    name: 'WEIL-TECHNOLOGY',
+    country: 'Germania',
+    description: 'Impianti e linee automatiche per formatura, taglio e giunzione.',
+    url: 'https://www.weil-engineering.de'
+  },
+  {
+    division: 'DIVMAC',
+    name: 'WF MASCHINENBAU',
+    country: 'Germania',
+    description: 'Macchine flow forming per componenti di trasmissione e deformazione lamiera.',
+    url: 'https://www.wf-maschinenbau.com'
+  },
+  {
+    division: 'DIVMAC',
+    name: 'LESSMANN',
+    country: 'Germania',
+    description: 'Spazzole industriali speciali per finitura, sbavatura e processi automatici.',
+    url: 'https://www.lessmann.com'
   }
 ];
 
@@ -114,7 +256,7 @@ const applications = [
 ];
 
 function render(page, data = {}) {
-  return { site, company, divisions, applications, page, ...data };
+  return { site, company, divisions, applications, partners, page, ...data };
 }
 
 app.get('/', (req, res) => res.render('pages/home', render('home')));
